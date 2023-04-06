aafc6c8305f8cb7f0789b732b385b5e2b7468255
dalloc2 <128258555+dalloc2@users.noreply.github.com>
2023-04-06 20:15:07 +0100

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,10 @@
+local Games = {
+  [5773982116] = "2PlayerPizzaTycoon",
+  [155615604] = "PrisonLife",
+}
+local PlaceId = game.PlaceId
+if Games[PlaceId] then 
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/dalloc2/Hub/Scripts/"..Games[PlaceId]..".lua"))()
+else
+  warn("you on a unsupported game mudafucka")
+end 
